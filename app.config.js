098fff5eca4c651c0cfdbd83c4300e5fe988ba01
Tega Umukoro <tegaumukoro@mac.mynetworksettings.com>
2025-10-03 23:50:02 -0500

    Bump build to 120 - Disable Google Sign-In crashes, debug Apple Sign-In, and edit modal debugging

--- app.config.js
+++ app.config.js
@@ -16,7 +16,7 @@ export default {
     ios: {
       supportsTablet: true,
       bundleIdentifier: process.env.IOS_BUNDLE_ID || 'com.jmotech.voyageai',
-      buildNumber: '119',
+      buildNumber: '120',
       icon: './assets/images/Resizedlogo.png',
       splash: {
         backgroundColor: '#FFFFFF'
