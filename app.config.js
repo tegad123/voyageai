00424147aa534c4c1edc6a2f4f3da31608d0c6b2
Tega Umukoro <tegaumukoro@mac.mynetworksettings.com>
2025-10-05 18:17:46 -0500

    Bump build number to 125

--- app.config.js
+++ app.config.js
@@ -16,7 +16,7 @@ export default {
     ios: {
       supportsTablet: true,
       bundleIdentifier: process.env.IOS_BUNDLE_ID || 'com.jmotech.voyageai',
-      buildNumber: '124',
+      buildNumber: '125',
       icon: './assets/images/Resizedlogo.png',
       splash: {
         backgroundColor: '#FFFFFF'
